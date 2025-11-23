288e48f316ee4206d89482224ef5e1b2cfa37288
Quester888 <tiniesgames12@gmail.com>
2025-11-23 17:44:31 +0000

    Refactor file collection into a separate function

--- listmo.py
+++ listmo.py
@@ -4,9 +4,14 @@ all_files = []
 
 
 
-for dirpath, dirnames, filenames in os.walk(root_dir):
-    for file in filenames:
-        full_path = os.path.join(dirpath, file)
-        all_files.append(full_path)
+def fn():
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for file in filenames:
+            full_path = os.path.join(dirpath, file)
+            all_files.append(full_path)
 
+
+fn()
 print(f"Found {len(all_files)} files.")
+
+
